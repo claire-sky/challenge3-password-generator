fc013fe58d847a0eac36d68f7ac523d23b0f316f
Claire Sky <eclconnolly@gmail.com>
2021-10-22 22:21:55 -0700

    Logic for password characters

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,7 @@
 // Assignment code here
 
 var passwordLength = 0
+var passwordCharacter = 0
 
 // Prompts for password criteria
 var lengthCriteria = function() {
@@ -22,6 +23,48 @@ var lengthCriteria = function() {
         }
     };
 };
+
+var characterCriteria = function() {
+  // Confirm character types (lowercase, uppercase, numeric, and/or special characters)
+  window.alert("Please select at least one character type: lowercase, uppercase, numeric, and/or special characters.");
+      
+    // Create lowercase check
+    var lowercaseCharacter = window.confirm("Do you want to include lowercase characters?")
+      if (lowercaseCharacter) {
+        console.log(lowercaseCharacter);
+      };
+
+    // Create uppercase check
+    var uppercaseCharacter = window.confirm("Do you want to include uppercase characters?")
+      if (uppercaseCharacter) {
+        console.log(uppercaseCharacter);
+      };
+
+    // Create numeric check
+    var numericCharacter = window.confirm("Do you want to include numbers?")
+      if (numericCharacter) {
+        console.log(numericCharacter);
+      };
+
+    // Create special characters check
+    var specialCharacter = window.confirm("Do you want to include special characters?")
+      if (specialCharacter) {
+        console.log(specialCharacter);
+      };
+
+    // Validate character type (minimum of 1)
+    passwordCharacter = [lowercaseCharacter, uppercaseCharacter, numericCharacter, specialCharacter];
+    var characterTypes = passwordCharacter.filter(Boolean).length;
+    if (characterTypes < 1) {
+      characterCriteria();
+    };
+
+    console.log(characterTypes);
+    console.log(passwordCharacter);
+
+    var characterCount = (passwordLength / characterTypes);
+    console.log(characterCount);
+  };
 
 // Click the button to generate a password
 var generatePassword = function() {
